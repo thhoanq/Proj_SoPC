5290eb15a32c0645f25e0acc06add33fa4c816bd
thhoanq <hoangtrinh140103@gmail.com>
2025-03-24 22:23:23 +0700

    update

---
+++ software/Security/obj/default/source.d
@@ -0,0 +1,11 @@
+obj/default/source.o: source.c ../Security_bsp//HAL/inc/io.h \
+ ../Security_bsp//HAL/inc/alt_types.h ../Security_bsp/system.h \
+ ../Security_bsp/linker.h
+
+../Security_bsp//HAL/inc/io.h:
+
+../Security_bsp//HAL/inc/alt_types.h:
+
+../Security_bsp/system.h:
+
+../Security_bsp/linker.h:
